c303ceb83c6190c9139cab3e2a8074326c2d2c46
Mayur Mahajan <mahajanmayur2111@gmail.com>
2026-06-04 23:32:11 +0530

    pakage

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ganak.ganak',
+  appId: 'com.ganak.counter',
   appName: 'Ganak',
   webDir: 'dist'
 };
